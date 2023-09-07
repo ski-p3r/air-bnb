c1ccfc7589b117913441c0232ee3a6882ca7aefc
Mubarek Shikur <manmuba90@gmail.com>
2023-09-08 00:41:10 +0300

    Button state

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -18,9 +18,29 @@ const Button: React.FC<ButtonProps> = ({
   icon,
 }) => {
   return (
-    <div>
-      <button>{label}</button>
-    </div>
+    <button
+      className={`
+            relative
+            disabled:opacity-70
+            disabled:cursor-not-allowed
+            rounded-lg
+            hover:opacity-80
+            transition 
+            w-full
+            ${
+              outline
+                ? "bg-white border-black text-black"
+                : "bg-rose-500 border-rose-500 text-white"
+            }
+            ${
+              small
+                ? "py-1 text-sm font-light border-[1px]"
+                : "py-3 text-md font-semibold border-2"
+            }
+        `}
+    >
+      {label}
+    </button>
   );
 };
 
