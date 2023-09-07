6d46feabeb7192b39f11cafe23feaa17beb4c31a
Mubarek Shikur <manmuba90@gmail.com>
2023-09-07 23:56:05 +0300

    Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import Navbar from './components/Navbar/Navbar';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/Navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modals from "./components/Modals/Modals";
 
-const font = Nunito({ subsets: ['latin'], })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
-          {children}</body>
+        <ClientOnly>
+          <Modals isOpen />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
